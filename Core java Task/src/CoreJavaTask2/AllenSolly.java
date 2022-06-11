75a52b8695bdbd4a273f145fde5103948032d010
Soma <soma>
2022-06-11 17:30:13 +0530

    Adding CoreJavaTask2

---
+++ Core java Task/src/CoreJavaTask2/AllenSolly.java
@@ -0,0 +1,16 @@
+package CoreJavaTask2;
+
+public class AllenSolly {
+	
+	public void Jeans() {
+		System.out.println("Denim Jeans");
+	}
+}
+
+class NewShopping extends AllenSolly{
+	
+	public void NewJeans() {
+		System.out.println("Ripped Jeans");
+	}
+}
+
