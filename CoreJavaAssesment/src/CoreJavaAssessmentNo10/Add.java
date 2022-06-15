9279edb78483abd60828e6b4f09b4eaadcb31174
Soma <somadas@techouts.co.in>
2022-06-15 19:39:37 +0530

    Core java Assessment no 10

---
+++ CoreJavaAssesment/src/CoreJavaAssessmentNo10/Add.java
@@ -0,0 +1,17 @@
+package CoreJavaAssessmentNo10;
+
+import java.util.Scanner;
+
+/**
+ * @author D1
+ *
+ */
+public class Add extends Thread{
+	
+	static Scanner sc = new Scanner(System.in);
+	static double d = sc.nextInt();
+	static double o = sc.nextInt();
+	 
+	
+
+}
