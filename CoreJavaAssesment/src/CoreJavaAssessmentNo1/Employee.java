2a6ef506307e97dceb3c4f94dfd547be91c93dc0
Soma <somadas@techouts.co.in>
2022-06-15 19:43:42 +0530

    Java assessment no 1

--- CoreJavaAssesment/src/CoreJavaAssessmentNo1/Employee.java
+++ CoreJavaAssesment/src/CoreJavaAssessmentNo1/Employee.java
@@ -2,16 +2,13 @@ package CoreJavaAssessmentNo1;
 
 public class Employee {
 	
-	private double rating;
-	private int startYear;
-	
-	void Employee(double rating , int startYear) {
-		this.rating = rating;
-		this.startYear = startYear;
-	}
-	public double getEmployeeRating() {
-		return this.rating;
-		
-	}
+	public double rating;
+	  public int startyear;
+	  
+	  Employee(double rating, int startyear)
+	  {
+	    this.rating = rating;
+	    this.startyear = startyear;
+	  }
 
 }
