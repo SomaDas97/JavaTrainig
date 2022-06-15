f43fa21c7bb74a0faa85a25fd8d4893c2cd04391
Soma <somadas@techouts.co.in>
2022-06-15 19:49:50 +0530

    java Assessment no 8

---
+++ CoreJavaAssesment/src/CoreJavaAssessmentNo8/Data.java
@@ -0,0 +1,28 @@
+package CoreJavaAssessmentNo8;
+
+//Using TreeSet, make sure to add varied data types to the set including Upper and Lower case and predict your output.
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Data {
+	
+	public static void main(String args[]) {
+		
+		Set ts = new TreeSet();
+		
+		ts.add(5);
+		ts.add(25);
+		ts.add(6);
+		
+		System.out.println(ts);
+		
+		
+		
+	
+	}
+	
+	
+
+}
