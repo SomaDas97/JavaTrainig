72b52d85199303cb217318b3318ceb29121a1ff5
Soma <somadas@techouts.co.in>
2022-06-15 19:47:05 +0530

    java Assessment no 20

---
+++ CoreJavaAssesment/src/CoreJavaAssessmentNo20/Iterators.java
@@ -0,0 +1,22 @@
+package CoreJavaAssessmentNo20;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//Methods of Iterator:hasNext(),next(),remove()
+
+public class Iterators {
+	
+	public static void main(String args[]) {
+		
+		List l = new ArrayList();
+		
+		Iterator it = l.iterator();
+		System.out.println(it.hasNext());
+		
+		
+		
+	}
+
+}
