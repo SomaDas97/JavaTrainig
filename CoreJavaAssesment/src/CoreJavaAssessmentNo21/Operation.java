ec35444a947b0f028d293ed1556d94a97dcdabce
Soma <somadas@techouts.co.in>
2022-06-15 19:48:28 +0530

    java Assessment no 21

---
+++ CoreJavaAssesment/src/CoreJavaAssessmentNo21/Operation.java
@@ -0,0 +1,26 @@
+package CoreJavaAssessmentNo21;
+
+public class Operation {
+	
+	final double plus = 1;
+	final double minus = 2;
+	final double divide = 3;
+	final double multiple = 4;
+	
+	public double calculate(int i , double a , double b) {
+		
+		if(i== plus) {
+			return(a+b);
+		}
+		else if (i== minus) {
+			
+			return(a-b);
+		}
+		else if(i== multiple) {
+			
+			return(a*b);
+		}
+		return 0;
+	}
+
+}
