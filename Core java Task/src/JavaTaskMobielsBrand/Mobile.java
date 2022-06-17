524ac0cd6f3e6a1c3b278596b79c62c2ba72eda7
Soma <somadas@techouts.co.in>
2022-06-17 17:14:03 +0530

    Core Java Mobiles

---
+++ Core java Task/src/JavaTaskMobielsBrand/Mobile.java
@@ -0,0 +1,15 @@
+package JavaTaskMobielsBrand;
+
+import java.util.Scanner;
+
+public class Mobile {
+	
+	public static void main(String args []) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DifferentBrands obj = new DifferentBrands();
+		obj.Phones();
+	} 
+
+}
